8dc3b4c35d98ae0e1963efb5522a1ab7b4eba8db
Sarah A <125934632+Sah11-0@users.noreply.github.com>
2024-12-21 14:36:07 -0500

    Update 1.1-is-unique.java

--- chapter-1/1.1-is-unique.java
+++ chapter-1/1.1-is-unique.java
@@ -1,4 +1,4 @@
-// 1.1 Implement an algorithm to determine if a string has all unique characters. What is you cannot use additional data structures?
+// 1.1 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 public class uniqueCharacters {
     public static boolean isUniqueChars(String str) {
